0682491e8d78767475c4e09432add18175b9b196
pakhuta_siarhei <pakhuta.siarhei@gmail.com>
2016-11-28 22:31:33 +0300

    task 1 -> fixed issues with bad request

--- src/app/map.ts
+++ src/app/map.ts
@@ -1,4 +1,4 @@
-import {vars} from './vars';
+import {VARS} from './vars';
 
 export class Map {
     currentPosition: google.maps.LatLng;
@@ -10,7 +10,7 @@ export class Map {
         private mapContainer: Element,
         private position: Position
     ) {
-        this.map = new google.maps.Map(this.mapContainer, vars.map.init);
+        this.map = new google.maps.Map(this.mapContainer, VARS.MAP.INIT);
         this.currentPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         this.map.setCenter(this.currentPosition);
     }
