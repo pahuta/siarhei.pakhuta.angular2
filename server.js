751da857109184068bbff92349a13101befc173c
pakhuta_siarhei <pakhuta.siarhei@gmail.com>
2016-12-24 20:46:34 +0300

    try to deploy 7

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8000;
+// var port = process.env.PORT || 8000;
+var port = 8000;
 
 app.use(express.static('./dist/'));
 app.listen(port);
