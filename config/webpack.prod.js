8acc437fd52d9487ff87d6c73d4b1ef36e407170
pakhuta.siarhei <pakhuta.siarhei@gmail.com>
2017-02-26 14:18:28 +0300

    task-7 -> try configure deploy 2

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig('production'), {
 
     output: {
         path: helper.rootPath("/dist/aot"),
-        publicPath: "dist/",
+        publicPath: "/",
         filename: "[name].js"
     },
 
